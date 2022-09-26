6a9e7048a81e62f7bd46227a35018b917d94b2c2
Emily Sellers <emilymelindasellers@gmail.com>
2022-09-26 15:01:04 -0700

    complete auth calls and redirect in auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -71,8 +71,16 @@ authForm.addEventListener('submit', async (e) => {
 
     // > Part A:
     //      - get formData object from form
+    const formData = new FormData(authForm);
     //      - call "authType.action" passing in the email and password from
     //        the form data and assign to response variable
+    let response = null;
+
+    if (authType === signInType) {
+        response = await signInUser(formData.get('email'), formData.get('password'));
+    } else {
+        response = await signUpUser(formData.get('email'), formData.get('password'));
+    }
 
     const error = response.error;
 
@@ -84,5 +92,6 @@ authForm.addEventListener('submit', async (e) => {
     } else {
         // go back to wherever user came from
         // > Part A using "location", replace url with "redirectUrl"
+        location.replace(redirectUrl);
     }
 });
